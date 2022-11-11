f9dc6bbe5af585a5a21c64db27fb00a6d489a3e4
soridalac <lacsorida@gmail.com>
2022-11-11 08:44:41 -0800

    add function to

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def duplicate(nums):
+    hashset = set()
 
+    for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+    return False
+
+print(duplicate([1, 2, 3, 1]))
